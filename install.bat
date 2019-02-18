b38edcffa46c127a48c6ff3bc8c3063c93c33b9c
2nd_junkey <broken.phantsm@gmail.com>
2019-02-18 23:55:02 +0900

    make installer

--- install.bat
+++ install.bat
@@ -1,1 +1,9 @@
 @echo off
+# install scoop and git
+powershell
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+iex (New-Object net.webclient).downloadstring('https://get.scoop.sh')
+
+scoop install git
+scoop update
+scoop update *
