4a3f7d0256be0995e7b9a2bb6437d2959191599b
2nd_junkey <broken.phantsm@gmail.com>
2019-02-24 22:27:15 +0900

    refactored

--- install.bat
+++ install.bat
@@ -1,8 +1,3 @@
 @echo off
 REM install scoop and git
-rem powershell -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force -Command "iex (New-Object net.webclient).downloadstring('https://get.scoop.sh')"
 powershell -ExecutionPolicy RemoteSigned -Command "iex (New-Object net.webclient).downloadstring('https://get.scoop.sh')"
-
-scoop install git
-scoop update
-scoop update *
