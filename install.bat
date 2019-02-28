8d447348d78664c250ff195dd0a0ff492fac576c
2nd_junkey <broken.phantsm@gmail.com>
2019-03-01 00:13:02 +0900

    fix finish comment

--- install.bat
+++ install.bat
@@ -3,5 +3,6 @@ REM install scoop and git
 powershell -ExecutionPolicy RemoteSigned -Command "iex (New-Object net.webclient).downloadstring('https://get.scoop.sh'); scoop install git; scoop update; scoop update *"
 
 rem wait user's action
-echo "finish install!"
+echo.
+echo finish install !
 timeout -1
