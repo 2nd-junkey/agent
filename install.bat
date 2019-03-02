da5ffc86549c10ec3f37cb2e54953e4cac14f269
2nd_junkey <broken.phantsm@gmail.com>
2019-03-02 23:41:41 +0900

    compliant to cp932

--- install.bat
+++ install.bat
@@ -2,6 +2,9 @@
 REM install scoop and git
 powershell -ExecutionPolicy RemoteSigned -Command "iex (New-Object net.webclient).downloadstring('https://get.scoop.sh'); scoop install git; scoop update; scoop update *"
 
+echo compiant to cp932(shift-JIS)
+powershell -Command "copy files\.bashrc ~"
+
 rem wait user's action
 echo.
 echo finish install !
